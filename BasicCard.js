c176fb62005a47d84e569a6f9de6ff9f6013f11c
Laura Unaeze <l.unaeze5@gmail.com>
2017-10-01 13:01:50 -0700

    Fixed app so it randomizes between possible question/answer pairs.

--- BasicCard.js
+++ BasicCard.js
@@ -15,17 +15,66 @@ function BasicCard(front, back) {
 	this.showBack = function () {
 		console.log(this.back);
 	};
+
+	//module to ask a question 
+	this.askQuestion = function (a) {
+		console.log("Question: " + this.front);
+		console.log("");
+		this.answerQuestion();
+	};
+
+	//module to answer a question. works in this.askQuestion above
+	this.answerQuestion = function () {
+		console.log("Answer: " + this.back);
+	}
 };
 
 
-//the new objects based off BasicCard
-var firstPresident = new BasicCard("Who was the first president of the United States?", "George Washington")
+//the new objects based off of constructor BasicCard
+var firstPresident = new BasicCard("Who was the first president of the United States?", "George Washington");
+var firstOnMoon = new BasicCard("Who was the first person on the moon?", "Neil Armstrong");
+var cheeseState = new BasicCard("What US state is known for its cheese?", "Wisconsin");
 
 
-//the export
+//an array to store the BasicCard objects
+var questionArray = [firstPresident, firstOnMoon, cheeseState];
+
+
+
+//================== the calls =================== 
+
+//firstPresident.showFront(); //test
+//firstPresident.showBack();  //test
+
+
+//the command after "node BasicCard.js" to run this file will be "ask"
+//these if/else-if statements below check against various entries
+//note to self: the order these are written in matters!!
+
+//if user doesn't enter a command
+if (!process.argv[2]) {
+	console.log("Please type in <<ask>> (without the carrots) after calling the app.");
+}
+
+//if user doesn't enter "ask" as the command
+else if (process.argv[2].toLowerCase() !== "ask") {
+	console.log("Please type in <<ask>> (without the carrots) after calling the app.");
+}
+
+//if user correctly enters "ask" as the command
+else if (process.argv[2].toLowerCase() === "ask") {
+	//randomly selects an object from questionArray and stores it in chosenQuestion
+	var chosenQuestion = questionArray[Math.floor(Math.random() * questionArray.length)];
+
+	//executes function with randomly selected object from questionArray
+	chosenQuestion.askQuestion();
+}
+
+//The necessary "else" to make if/else-if work
+else {
+	console.log("You have an error.");
+};
+
+
+//the export (for future use)
 module.exports = BasicCard;
-
-
-//the calls
-firstPresident.showFront();
-firstPresident.showBack();
